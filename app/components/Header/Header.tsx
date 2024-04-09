8a84db59b4f7d095b172a118e6b7992df01234f4
Alec Bana <anbana994@gmail.com>
2024-04-09 17:31:57 -0400

    added the basic dropdown functionality

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
+    const [isServicesDropdownOpen, setIsServicesDropdownOpen] = useState(false);
+
     return (
       <header className={styles.header}>
         <nav className={styles.navbar}>
@@ -15,12 +18,26 @@ const Header: React.FC = () => {
                 <a className={styles.navLink}>About Us</a>
               </Link>
             </li>
-            <li className={styles.navItem}>
-              <Link href="/dental-services">
-                <a className={styles.navLink}>Services</a>
-              </Link>
-              {/* Dropdown menu for Services */}
-            </li>
+            <li className={styles.navItem} onMouseEnter={() => setIsServicesDropdownOpen(true)} onMouseLeave={() => setIsServicesDropdownOpen(false)}>
+      <Link href="/dental-services">
+        <a className={styles.navLink}>Services</a>
+      </Link>
+      {isServicesDropdownOpen && (
+        <ul className={styles.dropdownMenu}>
+          <li className={styles.dropdownItem}>
+            <Link href="/dental-services/cleaning">
+              <a className={styles.dropdownLink}>Cleaning</a>
+            </Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link href="/dental-services/fillings">
+              <a className={styles.dropdownLink}>Fillings</a>
+            </Link>
+          </li>
+          {/* More dropdown items */}
+        </ul>
+      )}
+    </li>
             <li className={styles.navItem}>
               <Link href="/dental-blog">
                 <a className={styles.navLink}>Blog</a>
