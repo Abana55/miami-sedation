17b620b21690cefceaaf092246298e1053334c16
Alec Bana <anbana994@gmail.com>
2024-04-15 16:02:44 -0400

    adding styles and the footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,22 @@
-import dynamic from 'next/dynamic';
 import styles from './components/Home.module.scss';
 import Header from './components/Header/Header';
+import '../app/styles/globals.scss';
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.hero}>
-        <h1 className={styles.heroTitle}>Welcome to Miami Sedation Dentistry</h1>
-        <p className={styles.heroTagline}>Providing expert dental care with a gentle touch.</p>
-      </div>
-      <section className={styles.missionStatement}>
-        <h2 className={styles.sectionTitle}>Our Mission</h2>
-        <p className={styles.sectionContent}>
-          Our mission is to provide compassionate and comprehensive dental care to our patients, ensuring their comfort and well-being at every visit.
-        </p>
-      </section>
+   <div className={styles.container}>
+    <Header />
+    <div className={styles.hero}>
+      <h1 className={styles.heroTitle}>Welcome to Miami Sedation Dentistry</h1>
+      <p className={styles.heroTagline}>Providing expert dental care with a gentle touch.</p>
     </div>
+    <section className={styles.missionStatement}>
+      <h2 className={styles.sectionTitle}>Our Mission</h2>
+      <p className={styles.sectionContent}>
+        Our mission is to provide compassionate and comprehensive dental care to our patients, ensuring their comfort and well-being at every visit.
+      </p>
+    </section>
+  </div>
   );
 };
 
