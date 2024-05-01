2b6f6efd77055b857ed431008008b1d248b919f8
Alec Bana <anbana994@gmail.com>
2024-04-30 21:40:42 -0400

    added some mock content for the front page

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -6,32 +6,24 @@ import style from '../Dropdown/Dropdown.module.scss';
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-    <nav className={styles.navbar}>
-      <Link href="/" className={styles.link}>
-        Miami Sedation Dentistry
-      </Link>
-      <ul>
-        <li>
-          <Link href="/services" className={styles.link}>
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link href="/about-us" className={styles.link}>
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact-us" className={styles.link}>
-            Contact Us
-          </Link>
-        </li>
-        <li>
-          <Dropdown/>
-        </li>
-      </ul>
-    </nav>
-  </header>
+      <nav className={styles.navbar}>
+        <Link href="/" className={styles.link}>Miami Sedation Dentistry</Link>
+        <ul className={styles.navList}>
+          <li>
+            <Link href="/services" className={styles.link}>Services</Link>
+          </li>
+          <li>
+            <Link href="/about-us" className={styles.link}>About Us</Link>
+          </li>
+          <li>
+            <Link href="/contact-us" className={styles.link}>Contact Us</Link>
+          </li>
+          <li>
+            <Dropdown />
+          </li>
+        </ul>
+      </nav>
+    </header>
 );
 };
 
