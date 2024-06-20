2d1a6e1030a6e5160393ac7a36ba124d4d474ad3
Alec Bana <anbana994@gmail.com>
2024-06-19 23:56:14 -0400

    more lines added

--- app/dental-services/crowns/page.tsx
+++ app/dental-services/crowns/page.tsx
@@ -4,3 +4,19 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../../DentalServices.module.scss";
+
+
+const Crowns = () => {
+    return (
+      <>
+        <Head>
+          <title>Dental Crowns | Miami Sedation Dentistry</title>
+          <meta
+            name="description"
+            content="Learn about dental crowns provided by Miami Sedation Dentistry. Dental crowns are used to restore the function, shape, and appearance of damaged teeth."
+          />
+          <meta
+            name="keywords"
+            content="dental crowns, tooth restoration, dental care, oral health, Miami Sedation Dentistry"
+          />
+        </Head>
