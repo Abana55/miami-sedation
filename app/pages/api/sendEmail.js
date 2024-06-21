60637901f8bd5129bc5c1c610d2a7e87039cfe2c
Alec Bana <anbana994@gmail.com>
2024-06-21 19:27:27 -0400

    added api form

---
+++ app/pages/api/sendEmail.js
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email, phoneNumber, doctor, message, callbackDate, callbackTime } = req.body;
+
+    
+    let transporter = nodemailer.createTransport({
+      service: 'gmail', 
+      auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS, 
+      },
+    });
+
+    // Email content
+    let mailOptions = {
+      from: email,
+      to: process.env.OFFICE_EMAIL, // Office email address
+      subject: 'New Consultation Request',
+      text: `You have a new consultation request from:
+      
+      Name: ${name}
+      Email: ${email}
+      Phone Number: ${phoneNumber}
+      Doctor: ${doctor}
+      Message:
+      ${message}
+      
+      Callback Date: ${callbackDate}
+      Callback Time: ${callbackTime}
+      `,
+    };
+
+    try {
+      // Send email
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ success: true });
+    } catch (error) {
+      res.status(500).json({ error: 'Error sending email' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
