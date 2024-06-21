58f83965ed0409c95062973d91452b5bf992dc68
Alec Bana <anbana994@gmail.com>
2024-06-21 19:43:53 -0400

    added intial info for new page

---
+++ app/dental-services/veneers/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../DentalServices.module.scss";
+
+
+const Veneers = () => {
+    return (
+        <>
+        <Head>
+        <title>Dental Veneers | Bana Dental Design</title>
+        <meta
+          name="description"
+          content="Learn about the different types of dental veneers provided by Bana Dental Design. Dental veneers can enhance your smile by covering imperfections."
+        />
+        <meta
+          name="keywords"
+          content="dental veneers, cosmetic dentistry, smile enhancement, Bana Dental Design"
+        />
+      </Head>
+        </>
+    )
+
+
+export default Veneers;
