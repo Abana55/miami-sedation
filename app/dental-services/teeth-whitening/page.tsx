e4fc6bc5d2b9eb6ced9d4f186ed76b4d8837b363
Alec Bana <anbana994@gmail.com>
2024-06-27 19:51:11 -0400

    adding teeth whiting

---
+++ app/dental-services/teeth-whitening/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../../styles/DentalServices.module.scss";
+
+const TeethWhitening = () => {
+    return(
+        <Head>
+        <title>Teeth Whitening | Bana Dental Design</title>
+        <meta
+          name="description"
+          content="Learn about the professional teeth whitening services provided by Bana Dental Design. Achieve a brighter smile with our safe and effective teeth whitening options."
+        />
+        <meta
+          name="keywords"
+          content="teeth whitening, cosmetic dentistry, smile enhancement, Bana Dental Design"
+        />
+      </Head>
+    )
+}
+
+export default TeethWhitening;
