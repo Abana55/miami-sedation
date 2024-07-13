a2ecf9b7f0c0ac12bfa2fc315bb660cfb4a51f7d
Alec Bana <anbana994@gmail.com>
2024-07-12 23:12:41 -0400

    added a humberger icon

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,24 +1,57 @@
 "use client";
-import Link from 'next/link';
-import styles from './Header.module.scss';
-import Dropdown from '../Dropdown/Dropdown';
+
+import React, { useState } from "react";
+import Link from "next/link";
+import styles from "./Header.module.scss";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
-      <nav className={styles.navbar}>
-        <Link href="/" className={styles.brand}>
-          Bana Dental Design
+      <div className={styles.logo}>
+        <Link href="/">
+        Bana Dental Design
         </Link>
-        <ul className={styles.navList}>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={isMenuOpen ? styles.hamburgerOpen : ""}></div>
+      </div>
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ""}`}>
+        <ul>
           <li>
-            <Link href="/about-us" className={styles.link}>About Us</Link>
+            <Link href="/about-us">
+              About Us
+            </Link>
           </li>
           <li>
-            <Link href="/contact-us" className={styles.link}>Contact Us</Link>
+            <Link href="/contact-us">
+              Contact Us
+            </Link>
           </li>
-          <li>
-            <Dropdown />
+          <li className={styles.dropdown}>
+            Services
+            <ul className={styles.dropdownMenu}>
+              <li>
+                <Link href="/services/service1">
+                  Service 1
+                </Link>
+              </li>
+              <li>
+                <Link href="/services/service2">
+                  Service 2
+                </Link>
+              </li>
+              <li>
+                <Link href="/services/service3">
+                  Service 3
+                </Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
