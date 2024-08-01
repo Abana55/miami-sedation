bbe0a0bb74156108c352c5d38cfdade97c8b3797
Alec Bana <anbana994@gmail.com>
2024-08-01 17:53:45 -0400

    styling

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -12,34 +12,37 @@ const Footer = () => {
         <Image
           src="/images/1x/3Artboard 1_1.png" // Update with your logo image path
           alt="Company Logo"
-          layout="fill"
-          objectFit="contain"
+          layout="responsive"
+          width={96}
+          height={96}
           className={styles.footerLogo}
         />
       </div>
       <div className={styles.footerRight}>
-        <div className={styles.footerColumn}>
-          <h3>Hours</h3>
-          <p>Mon: 9:30am-7:00pm</p>
-          <p>Tues-Thurs: 8:45am-6:00pm</p>
-          <p>Fri: 9:00am-5:00pm</p>
-        </div>
-        <div className={styles.footerColumn}>
-          <h3>Stay in Touch</h3>
-          <p>
-            <a href="mailto:info@banadental.com">info@banadental.com</a>
-          </p>
-          <Link href="/contact-us">
-            <button className={styles.footerButton}>Contact Us</button>
-          </Link>
-        </div>
-        <div className={styles.footerColumn}>
-          <h3>Address</h3>
-          <p>2461 Coral Way, Suite 100</p>
-          <p>Miami, FL 33145</p>
-          <p>
-            <a href="tel:+17866255550">(786) 625-5550</a>
-          </p>
+        <div className={styles.footerSections}>
+          <div className={styles.footerColumn}>
+            <h3>Hours</h3>
+            <p>Mon: 9:30am-7:00pm</p>
+            <p>Tues-Thurs: 8:45am-6:00pm</p>
+            <p>Fri: 9:00am-5:00pm</p>
+          </div>
+          <div className={styles.footerColumn}>
+            <h3>Stay in Touch</h3>
+            <p>
+              <a href="mailto:info@banadental.com">info@banadental.com</a>
+            </p>
+            <Link href="/contact-us">
+              <button className={styles.footerButton}>Contact Us</button>
+            </Link>
+          </div>
+          <div className={styles.footerColumn}>
+            <h3>Address</h3>
+            <p>2461 Coral Way, Suite 100</p>
+            <p>Miami, FL 33145</p>
+            <p>
+              <a href="tel:+17866255550">(786) 625-5550</a>
+            </p>
+          </div>
         </div>
         <div className={styles.footerBottom}>
           <p>©2024 Dr. Ramon Bana. All Rights Reserved.</p>
