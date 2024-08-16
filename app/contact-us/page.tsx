790c5111586f27df66c5cf7eeb6026e570a88dba
Alec Bana <anbana994@gmail.com>
2024-08-16 18:56:18 -0400

    sstyling

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -12,73 +12,80 @@ import ContactFormComponent from "../components/ContactFormComponent/ContactForm
 function ContactPage() {
   return (
     <div className={styles.pageContainer}>
-      <section className={styles.infoSection}>
-        <h2 className={styles.headerText}>We're so glad you're here!</h2>
-        <p className={styles.additionalInfo}>
-          This may be the first step in your oral health journey - And we want
-          to make your experience with us <span>the best you've ever had.</span>
-        </p>
-        <div className={styles.socialMediaLinks}>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              size="2x"
-              className={styles.socialIcon}
-            />
-          </a>
-          <a
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faFacebook}
-              size="2x"
-              className={styles.socialIcon}
-            />
-          </a>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faTwitter}
-              size="2x"
-              className={styles.socialIcon}
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              size="2x"
-              className={styles.socialIcon}
-            />
-          </a>
-          <a
-            href="https://www.youtube.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faYoutube}
-              size="2x"
-              className={styles.socialIcon}
-            />
-          </a>
-        </div>
-      </section>
-      <section className={styles.formSection}>
-        <ContactFormComponent />
-      </section>
+      <div className={styles.row}>
+        {/* Info Section */}
+        <section className={styles.infoSection}>
+          <h2 className={styles.headerText}>We're so glad you're here!</h2>
+          <p className={styles.additionalInfo}>
+            This may be the first step in your oral health journey - And we want
+            to make your experience with us <span>the best you've ever had.</span>
+          </p>
+          <div className={styles.socialMediaLinks}>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faInstagram}
+                size="2x"
+                className={styles.socialIcon}
+              />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faFacebook}
+                size="2x"
+                className={styles.socialIcon}
+              />
+            </a>
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faTwitter}
+                size="2x"
+                className={styles.socialIcon}
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size="2x"
+                className={styles.socialIcon}
+              />
+            </a>
+            <a
+              href="https://www.youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faYoutube}
+                size="2x"
+                className={styles.socialIcon}
+              />
+            </a>
+          </div>
+        </section>
+
+        {/* Form Section */}
+        <section className={styles.formSection}>
+          <ContactFormComponent />
+        </section>
+      </div>
+
+      {/* Banner Section */}
       <div className={styles.connectBanner}>
         <h1 className={styles.bannerTitle}>
           PERFECTION IN EVERY <span className={styles.goldText}>SMILE</span>
