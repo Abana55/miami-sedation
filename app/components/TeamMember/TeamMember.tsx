e897526dd1bf4c9b1c7bfd53901f6e4fc3c686f5
Alec Bana <anbana994@gmail.com>
2024-10-01 20:05:01 -0400

    code for component

---
+++ app/components/TeamMember/TeamMember.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import styles from "./TeamMember.module.scss";
+
+interface TeamMemberProps {
+  imageSrc: string;
+  altText: string;
+  name: string;
+  position: string;
+  bio: string[];
+}
+
+const TeamMember: React.FC<TeamMemberProps> = ({
+  imageSrc,
+  altText,
+  name,
+  position,
+  bio,
+}) => {
+  return (
+    <div className={styles.teamMember}>
+      <Image
+        src={imageSrc}
+        alt={altText}
+        width={200}
+        height={300}
+        className={styles.image}
+      />
+      <div className={styles.teamText}>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.position}>{position}</p>
+        {bio.map((paragraph, index) => (
+          <p key={index} className={styles.bio}>
+            {paragraph}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TeamMember;
