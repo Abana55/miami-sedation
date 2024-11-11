75b93f2266cc99a432d4fecc4b605b6206e5f3b1
Alec Bana <anbana994@gmail.com>
2024-11-11 18:40:11 -0500

    styling

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,7 +5,6 @@ import TeamMember from "../components/TeamMember/TeamMember";
 import styles from "./AboutUs.module.scss";
 import React from "react";
 
-
 interface TeamMemberProps {
   imageSrc: string;
   altText: string;
@@ -13,11 +12,10 @@ interface TeamMemberProps {
   position: string;
   bio: string[];
   isDoctor?: boolean;
-  className?: string; 
+  className?: string;
 }
 
 const AboutPage: NextPage = () => {
-  
   // const teamMembers: TeamMemberData[] = [
   //   {
   //     id: 1,
@@ -215,9 +213,9 @@ const AboutPage: NextPage = () => {
             name="Dr. Ramon Bana"
             position="Doctor of Dental Surgery"
             bio={[
-              "Dr. Ramon Bana was born in La Habana, Cuba...",
-              "Bana’s patients describe him as down-to-earth...",
-              "Dr. Bana has continued his education for the past 33 years...",
+              `Dr. Ramon Bana was born in La Habana, Cuba and came to Miami at as a young boy. He graduated from Hialeah High School (Class of 1981) and went on to study at the number one dental school at the time, Marquette University in Milwaukee, Wisconsin. He graduated as a Doctor of Dental Surgery in 1991. Over the last 33 years, he has taken countless continuing education courses including implant and full arch dentistry courses, orthodontic courses, digital smile design courses, and even therapeutic and cosmetic botox courses to keep up with the ever growing technology in the field of dentistry.
+                     Bana’s patients describe him as down-to-earth, professional, and full of heart. Patient comfort is his top priority, and patients say they feel like extended family. He always strives to leave patients feeling better than when they arrived. He is dedicated to his practice, patients, and team  and it shows. Dr. Bana's passion for dentistry shows vastly through his work as his goal is always to provide only the top quality dentistry for his patients.
+                     Dr. Bana has continued his education for the past 33 years ensuring he is bringing the best of the best to his patients. He promises excellence and does the work to make sure he delivers on that promise, and patients always leave his care healthier, happier, and with brighter smiles.`,
             ]}
             isDoctor={true}
             className={styles.doctorProfile}
@@ -229,9 +227,10 @@ const AboutPage: NextPage = () => {
             name="Dr. Andrew Brattain"
             position="Doctor of Dental Medicine"
             bio={[
-              "Dr. Andrew Brattain was born and raised in Miami...",
-              "Dr. Brattain and his wife Danielle began their professional careers...",
-              "Dr. Brattain is very excited to join Dr. Bana and the team...",
+              `Dr. Andrew Brattain was born and raised in Miami, FL and graduated from Christopher Columbus High School (Class of 2008). He graduated from the University of Florida in 2012, where he received his Bachelor of Science in
+               Nutritional Sciences. He then moved to Boston and graduated from the Boston University School of Dental Medicine in 2016 with summa cum laude honors. Dr. Brattain and his wife Danielle began their professional
+              careers in Tampa, FL. While in Tampa, they welcomed their first-born baby girl Olivia. This past year, they decided to finally move back home to Miami and are now expecting the birth of their baby boy. Dr. Brattain is very excited to join Dr. Bana and the amazing team at Miami Sedation and Cosmetic Dentistry. He believes first and foremost in the importance of building a strong relationship with each of his patients. This creates a foundation that allows for personalized treatment to achieve the best patient experience and
+               oral health possible. Through various forms of education and experience, Dr. Brattain constantly stays up to date with the latest advancements in dental treatment and looks forward to utilizing these with all his patients.`,
             ]}
             isDoctor={true}
             className={styles.doctorProfile}
@@ -257,6 +256,21 @@ const AboutPage: NextPage = () => {
               "Sandra brings over a decade of experience as a dental assistant...",
             ]}
           />
+          <TeamMember
+          imageSrc="/images/aboutUs/Lauren.jpg"
+          altText="Lauren Bana"
+          name="Lauren Bana"
+          position="resident loser"
+          bio={[`she sits there and stares at her phone`]}
+          />
+          <TeamMember
+          imageSrc="/images/aboutUs/Lourdes.jpg"
+          altText="Lourdes Bana"
+          name="Lourdes Bana"
+          position="Human Resources"
+          bio={[`makes everyone happy`]}
+          />
+
         </div>
       </section>
       <ConsultationBanner />
